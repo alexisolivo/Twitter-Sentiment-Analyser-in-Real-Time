2d5062e85b53098ca76421773d78abc32aab7b18
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-04-29 19:55:25 +0100

    Added Error Handling in Producer code
    Now error handling is done to prevent custom errors being thrown when twitter auth has failed or when Kafka servers are not accessible. Users can now clearly understand when the twitter auth is invalid or if the Kafka server is down.

--- producer.py
+++ producer.py
@@ -1,5 +1,6 @@
 import json
 # Twitter API Access Keys
+import datetime
 
 import tweepy
 from tweepy import OAuthHandler
@@ -7,17 +8,51 @@ from tweepy import Stream
 from kafka import KafkaProducer
 import twitter_creds as tc
 
-WORDS_TO_TRACK = ["Covid-19","coronavirus","covid"]
+import tweepy
+import twitter_creds as tc
 
+def verify_access():
+    # Authenticate to Twitter
+    auth = tweepy.OAuthHandler(tc.consumer_key, 
+        tc.consumer_secret)
+    auth.set_access_token(tc.access_token, 
+        tc.access_token_secret)
+    api = tweepy.API(auth)
+    try:
+        status = api.verify_credentials()
+        if status:
+            print("\nTwitter Authentication Successful.\n")
+            return True
+        else:
+            print('\nInvalid Twitter Credentials.\n')
+            return False
+    except:
+        print("\nError during Twitter Authentication.\n")
+        return False
+
+def check_broker_status():
+    kafka_producer = ''
+    kafka_producer_server = KafkaConfig().get_producer_sever()
+    try:
+        kafka_producer = KafkaProducer(bootstrap_servers=kafka_producer_server)
+        print('Kafka brokers are running at 9092.')
+        return True
+    except:
+        print('No Kafka brokers are running at 9092. Please start Kafka server.')
+        print('\nExiting....\n')
+        return False
+
+
+WORDS_TO_TRACK = ["Covid-19","coronavirus","covid"]
 
 class KafkaConfig():
     def __init__(self):
-        self.producer = KafkaProducer(bootstrap_servers='localhost:9092') #Same port as your Kafka server
+        self.producer_sever = 'localhost:9092' #Same port as your Kafka server
 
-    def get_producer(self):
-        return self.producer
+    def get_producer_sever(self):
+        return self.producer_sever
 
-class TwitterAuthConfig():
+class TwitterConfig():
     def __init__(self):
         self._consumer_key = tc.consumer_key
         self._consumer_secret = tc.consumer_secret
@@ -43,46 +78,11 @@ class TwitterAuth():
 
     def authenticate_twitter_app(self):
 
-        twitter_config = TwitterAuthConfig()
+        twitter_config = TwitterConfig()
         auth = OAuthHandler(twitter_config.get_consumer_key(), twitter_config.get_consumer_secret())
         auth.set_access_token(twitter_config.get_access_token(), twitter_config.get_access_token_secret())
         return auth
-
-class ListenerTS(tweepy.StreamListener):
-   
-    def on_data(self, data):
-            flag = True
-            kafka_producer = KafkaConfig().get_producer()
-            topic_name = "twitterdata"
-            tweet = json.loads(data)
-            tweet_text = ""
-            #Check if the tweet has the keys lang and created_at and whether its in english
-            if all(x in tweet.keys() for x in ['lang', 'created_at']) and tweet['lang'] == 'en':
-                if 'retweeted_status' in tweet.keys():
-                    if 'quoted_status' in tweet['retweeted_status'].keys():
-                        if('extended_tweet' in tweet['retweeted_status']['quoted_status'].keys()):
-                            tweet_text = tweet['retweeted_status']['quoted_status']['extended_tweet']['full_text']
-                        elif 'extended_tweet' in tweet['retweeted_status'].keys():
-                            tweet_text = tweet['retweeted_status']['extended_tweet']['full_text']
-                elif tweet['truncated'] == 'true':
-                    tweet_text = tweet['extended_tweet']['full_text']
-
-                else:
-                    tweet_text = tweet['text']
-
-            if tweet_text:
-                data = {
-                    'created_at': tweet['created_at'],
-                    'tweet': tweet_text
-                }
-
-                kafka_producer.send(topic_name, value = json.dumps(data).encode('utf-8'))
-    
-    def on_error(self, status):
-        # 420 error happens due to rate limiting
-        if(status == 420): 
-            #returning False in on_data disconnects the stream
-            return False         
+        
 
 class TwitterStreamer():
 
@@ -93,15 +93,58 @@ class TwitterStreamer():
     def stream_tweets(self):
         i = 0
         while True:
-            i+=1
-            print('{0}.Hello'.format(i))
             listener = ListenerTS() 
             auth = self.TwitterAuth.authenticate_twitter_app()
             stream = Stream(auth, listener)
             stream.filter(track = WORDS_TO_TRACK, stall_warnings=True, languages= ["en"])
-            print(stream)
+         
+
+
+class ListenerTS(tweepy.StreamListener):
+   
+    def on_data(self, data):
+
+            kafka_producer = ''
+            kafka_producer_server = KafkaConfig().get_producer_sever()
+
+            kafka_producer = KafkaProducer(bootstrap_servers=kafka_producer_server)
+                
+            if kafka_producer != '':
+                topic_name = "twitterdata"
+                tweet = json.loads(data)
+                tweet_text = ""
+                #Check if the tweet has the keys lang and created_at and whether its in english
+                if all(x in tweet.keys() for x in ['lang', 'created_at']) and tweet['lang'] == 'en':
+                    if 'retweeted_status' in tweet.keys():
+                        if 'quoted_status' in tweet['retweeted_status'].keys():
+                            if('extended_tweet' in tweet['retweeted_status']['quoted_status'].keys()):
+                                tweet_text = tweet['retweeted_status']['quoted_status']['extended_tweet']['full_text']
+                            elif 'extended_tweet' in tweet['retweeted_status'].keys():
+                                tweet_text = tweet['retweeted_status']['extended_tweet']['full_text']
+                    elif tweet['truncated'] == 'true':
+                        tweet_text = tweet['extended_tweet']['full_text']
+
+                    else:
+                        tweet_text = tweet['text']
+
+                if tweet_text:
+                    data = {
+                        'created_at': tweet['created_at'],
+                        'tweet': tweet_text
+                    }
+
+                    kafka_producer.send(topic_name, value = json.dumps(data).encode('utf-8'))
+                    print(f"\nTweet has been send to Kafka topic at: {datetime.datetime.now()}")
+
+    def on_error(self, status):
+        # 420 error happens due to rate limiting
+        if(status == 420): 
+            #returning False in on_data disconnects the stream
+            return False 
       
 
 if __name__ == "__main__":
-    TS = TwitterStreamer()
-    TS.stream_tweets()
+    
+    if verify_access() and check_broker_status():
+        TS = TwitterStreamer()
+        TS.stream_tweets()
